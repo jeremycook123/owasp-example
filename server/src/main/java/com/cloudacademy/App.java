c121d1bf671957be14f009541bd799b8290f130b
Jeremy Cook <jeremy.cook@cloudacademy.com>
2022-06-15 14:53:04 +1200

    added version logging

--- server/src/main/java/com/cloudacademy/App.java
+++ server/src/main/java/com/cloudacademy/App.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App {
 	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
-
-		System.out.println("app starting...");
-	}
+		SpringApplication springApplication = new SpringApplication(App.class);
+		springApplication.addListeners(new VersionLogger());
+		springApplication.run(args);
+   }
 }
+
+
+
