09f6bea1296ed86ccbd4cbc0230ac3cd8dee3bc7
Jeremy Cook <jeremy.cook@cloudacademy.com>
2022-06-15 12:57:48 +1200

    updates

--- server/src/main/java/com/cloudacademy/controllers/LoginController.java
+++ server/src/main/java/com/cloudacademy/controllers/LoginController.java
@@ -7,26 +7,32 @@ import com.cloudacademy.LoginResponse;
 import com.cloudacademy.Unauthorized;
 import com.cloudacademy.jwt.TokenManagement;
 import com.cloudacademy.User;
-import com.cloudacademy.database.Postgres;
+import com.cloudacademy.crypto.Hash;
 
 @RestController
 @EnableAutoConfiguration
 public class LoginController {
-	@CrossOrigin(origins = "*")
-    @RequestMapping(value = "/login", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+	
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value = "/login", 
+                    method = RequestMethod.POST, 
+                    produces = "application/json", 
+                    consumes = "application/json")
     LoginResponse login(@RequestBody LoginRequest input) {
         System.out.println("login POST called...");
         var authenticated = false;
 
         User user = User.fetch(input.username);
+
         System.out.println("database hash=" + user.hashedPassword);
-        System.out.println("java hash=" + Postgres.md5(input.password));
-        if (Postgres.md5(input.password).equals(user.hashedPassword)) {
+        System.out.println("java hash=" + Hash.md5(input.password));
+
+        if (Hash.md5(input.password).equals(user.hashedPassword)) {
           authenticated = true;
         } 
 
         if (authenticated) {
-            System.out.println("username: " + input.username);
+            System.out.println("authenticated successfully: " + input.username);
             String sessionToken = TokenManagement.createJWTToken(input.username);
             return new LoginResponse(sessionToken);
         } else {
--- server/src/main/java/com/cloudacademy/database/Postgres.java
+++ server/src/main/java/com/cloudacademy/database/Postgres.java
@@ -2,15 +2,13 @@ package com.cloudacademy.database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 public class Postgres {
 
     public static Connection connection() {
         try {
             Class.forName("org.postgresql.Driver");
-            String url = System.getenv("POSTGRES_CONNSTR");
+            var url = System.getenv("POSTGRES_CONNSTR");
             System.out.println("postgres connection string: " + url);
             return DriverManager.getConnection(url,
                                                 System.getenv("POSTGRES_USER"), 
@@ -22,36 +20,4 @@ public class Postgres {
         }
         return null;
     }
-
-    // Java program to calculate MD5 hash value
-    public static String md5(String input)
-    {
-        try {
-            MessageDigest digester = MessageDigest.getInstance("MD5");
-            digester.update(input.getBytes());
-            byte[] md5Bytes = digester.digest();
-            String md5Text = null;
-        
-            md5Text = bytesToHex(md5Bytes);
-        
-            return md5Text;
-        }
-
-        // For specifying wrong message digest algorithms
-        catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static final char[] hexArray = "0123456789abcdef".toCharArray();
-
-    private static String bytesToHex(byte[] bytes) {
-        char[] hexChars = new char[bytes.length * 2];
-        for (int j = 0; j < bytes.length; j++) {
-            int v = bytes[j] & 0xFF;
-            hexChars[j * 2] = hexArray[v >>> 4];
-            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
-        }
-        return new String(hexChars);
-    }    
 }
